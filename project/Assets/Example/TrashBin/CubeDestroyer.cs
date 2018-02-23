3b5a4940df3dc4cf2997b7d75b5fabaca549c91f
DESKTOP-SCNHE0V\Karona <tournkarona@gmail.com>
2018-02-23 22:47:44 +0700

    - added project

---
+++ project/Assets/Example/TrashBin/CubeDestroyer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class CubeDestroyer : MonoBehaviour
+{
+	private void OnCollisionEnter ( Collision collision )
+	{
+		TK.TrashBins.TrashBinManager.TakeIn ( collision.gameObject );
+	}
+}
---
+++ project/Assets/Example/TrashBin/CubeSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	private void SpawnCube ()
+	{
+		TK.TrashBins.TrashBinManager.Get ( "trash-bin" ).TakeOut ( new TK.TrashBins.TrashConfig () { binName = "cube", position = transform.position, isActive = true, rotation = Quaternion.Euler ( Random.value * 360f, Random.value * 360f, Random.value * 360f ) } );
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if ( Input.anyKeyDown )
+		{
+			SpawnCube ();
+		}
+	}
+}
